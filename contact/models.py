2879dd2e3581d6cf878ece7f1eb46211c9a37359
Sérgio Dantas <sergiodnts828@gmail.com>
2023-08-22 20:18:10 -0300

    configure media url and media root

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to=r'pictures/%Y/%m/%d/', blank=True)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}' if self.last_name else self.first_name
